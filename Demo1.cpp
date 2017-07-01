c1d993273498217f0e889ae909fea3c3c4e4e7c2
steve <551619855@qq.com>
2017-07-01 15:31:12 +0800

    xxx

--- Demo1.cpp
+++ Demo1.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     Widget w;
-    w.setGeometry(500, 200, 320, 240);
+    w.setGeometry(700, 500, 320, 240);
     w.show();
     return a.exec();
 }
